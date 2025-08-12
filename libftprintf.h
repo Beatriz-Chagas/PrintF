3de73b9c0e76800fbc1e7d93b118220d134e3810
Beatriz Chagas De Sousa <bchagas-@c2r2p2.42sp.org.br>
2025-08-12 08:45:29 -0300

    PrintF iniciada

---
+++ libftprintf.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bchagas- <bchagas-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 05:30:34 by bchagas-          #+#    #+#             */
+/*   Updated: 2025/08/12 07:35:39 by bchagas-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putnbr(int n);
+int	ft_puthex(unsigned long n, char upper);
+int	f_putptr(void *ptr);
+int ft_printf(const char *format, ...);
+#endif
